18d9a56ea5fae3f0325c94faa9c2ca125818260e
kshshton <michkrzyszton@gmail.com>
2026-06-25 21:44:31 +0200

    Save posts to db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,19 @@ package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.*;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-class Main {
-    static class Response {
+public class Main {
+    public static class Response {
         public List<Post> posts;
         public int total;
         public int skip;
@@ -29,7 +32,7 @@ class Main {
         }
     }
 
-    static class Post {
+    public static class Post {
         public int id;
         public String title;
         public String body;
@@ -44,11 +47,58 @@ class Main {
                     "id=" + id +
                     ", title='" + title + '\'' +
                     ", tags=" + tags +
+                    ", reactions=" + reactions +
                     ", views=" + views +
+                    ", userId=" + userId +
                     '}';
         }
     }
 
+    public static class Database {
+        private final Connection conn;
+
+        public Database() throws SQLException {
+            this("jdbc:sqlite:app.db");
+        }
+
+        public Database(String url) throws  SQLException {
+            this.conn = DriverManager.getConnection(url);
+
+            createTable();
+        }
+
+        private void createTable() throws SQLException {
+            String sql = """
+                CREATE TABLE IF NOT EXISTS posts (
+                    id INTEGER PRIMARY KEY,
+                    title TEXT NOT NULL,
+                    tags TEXT,
+                    views INTEGER,
+                    userId INTEGER
+                );
+                """;
+
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);
+            }
+        }
+
+        public void savePost(Post post) throws SQLException {
+            String sql = """
+                INSERT INTO posts (id, title, tags, views, userId)
+                VALUES (?, ?, ?, ?, ?);
+                """;
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setInt(1, post.id);
+                stmt.setString(2, post.title);
+                stmt.setString(3, post.tags.toString());
+                stmt.setInt(4, post.views);
+                stmt.setInt(5, post.userId);
+                stmt.executeUpdate();
+            }
+        }
+    }
 
     void main() {
         HttpClient client = HttpClient.newHttpClient();
@@ -57,6 +107,13 @@ class Main {
                 .GET()
                 .build();
         HttpResponse<String> response = null;
+        Database db = null;
+
+        try {
+            db = new Database();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -71,10 +128,10 @@ class Main {
 
         try {
             Response data = mapper.readValue(content, Response.class);
-            Post firstPost = data.posts.get(0);
-            System.out.println(firstPost);
-
-        } catch (JsonProcessingException e) {
+            for (Post post : data.posts) {
+                db.savePost(post);
+            }
+        } catch (JsonProcessingException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
